a10de6be62be83299178fee2701b5d1f893735de
mehak187 <mehakamir187@gmail.com>
2023-04-06 17:15:50 +0500

    update popup

--- resources/views/admin/customerDetails.blade.php
+++ resources/views/admin/customerDetails.blade.php
@@ -204,6 +204,11 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <script src="{{ asset('/js/main.js') }}"></script>
+    <style>
+        input::placeholder{
+            opacity:0.6 !important;
+        }
+    </style>
 </body>
 
 </html>
