231584cc6d5d2e8ba0f3dff922d1d948ad53f15d
Butterfly <mehak-fabtechsol>
2023-07-26 05:41:54 -0700

    mobile msg autofill and customer  back page error

--- app/Http/Controllers/GymController.php
+++ app/Http/Controllers/GymController.php
@@ -143,15 +143,15 @@ class GymController extends Controller
             ]);
         }
         // --------send-msg----
-        $accountSid = getenv("TWILIO_SID");
-        $authToken = getenv("TWILIO_AUTH_TOKEN");
-        $twilioNumber = getenv("TWILIO_NUMBER");
-        $client = new Client($accountSid, $authToken);
-        $client->messages->create("+923041461400", [
-            'From' => $twilioNumber,
-            'body' => $req->msg
-        ]);
-  
+        // $accountSid = getenv("TWILIO_SID");
+        // $authToken = getenv("TWILIO_AUTH_TOKEN");
+        // $twilioNumber = getenv("TWILIO_NUMBER");
+        // $client = new Client($accountSid, $authToken);
+        // $client->messages->create("+923041461400", [
+        //     'From' => $twilioNumber,
+        //     'body' => $req->msg
+        // ]);
+   
         return redirect('/newMember')->with([
             'success' => 'Member added successfully',
             'requestBarcode' => $requestMail,
